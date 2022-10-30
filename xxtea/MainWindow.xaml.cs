d1a4794de9b8b7ab039d434317c691f1da262fa4
i.demchuk <is.demchuk@gmail.com>
2022-10-30 13:19:14 +0700

    form

--- xxtea/MainWindow.xaml.cs
+++ xxtea/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace xxtea
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string inputFilePath;
+        private string outputFilePath;
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void InputFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+            openFileDlg.DefaultExt = ".txt";
+            openFileDlg.Filter = "Text documents (.txt)|*.txt";
+
+            // Launch OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
+            if (result == true)
+            {
+                InputFileTextbox.Text = openFileDlg.FileName;
+                inputFilePath = System.IO.File.ReadAllText(openFileDlg.FileName);
+            }
+        }
+
+        private void OutputFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+            openFileDlg.DefaultExt = ".txt";
+            openFileDlg.Filter = "Text documents (.txt)|*.txt";
+
+            // Launch OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
+            if (result == true)
+            {
+                OutputFileTextbox.Text = openFileDlg.FileName;
+                outputFilePath = System.IO.File.ReadAllText(openFileDlg.FileName);
+            }
         }
     }
 }
+
